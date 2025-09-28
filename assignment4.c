ef2fafc15ec309750ae8032dc08ec7bc03df5280
Taha Javed <k253053@nu.edu.pk>
2025-09-28 07:36:12 -0700

    Add files via upload

---
+++ assignment4.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+int main(){
+
+int userequal1=0;
+char enteredusername[50]="";
+char username[50]="TahaJaved";
+char password[50]="tahajaved23";
+char enteredpassword[50]="";
+int passequal = 0;
+
+printf("enter your username: ");
+fgets(enteredusername, sizeof(enteredusername),stdin);
+enteredusername[strcspn(enteredusername, "\n")] = 0;
+userequal1 = strcmp(username,enteredusername);
+
+if(userequal1==0){
+printf("enter password: ");
+fgets(enteredpassword, sizeof(enteredpassword),stdin);
+enteredpassword[strcspn(enteredpassword, "\n")] = 0;
+int passequal = strcmp(password, enteredpassword);
+
+if(passequal==0){
+printf("logged in");
+}
+else{
+    printf("incorrect password");
+}
+}
+else{
+    printf("username not found");
+}
+
+
+
+
+
+    return 0;
+}
+/*
+PSEUDOCODE:
+IF username is correct THEN
+IF password is correct THEN
+PRINT "Login successful"
+ELSE
+PRINT "Incorrect password"
+ENDIF
+ELSE
+PRINT "Username not found"
+ENDIF
+*/
